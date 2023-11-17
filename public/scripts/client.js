c7129e28dc320e2653e040f3c7e435220c21c7ba
Gerald <gerald.mwang@gmail.com>
2023-11-16 17:31:43 -0700

    implemented form submission with jquery, form validation, tweet refetching on submission, display of validation errors and tweet styling.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,103 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
 
+  const $characterCount = $("#charcount");
+
+
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  // Function to render Tweets from database
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $(".tweet-list").append($tweet);
+    }
+  };
+  // renderTweets(tweetData);
+
+  // Create new tweet
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`
+    <article class="tweet">
+    <header>
+      <div class="tweet-avatar">
+        <img src=${(tweet.user.avatars)}>
+        <p>${(tweet.user.name)}</p>
+      </div>
+      <p class="tweet-handle">${(tweet.user.handle)}</p>
+    </header>
+    <p class="tweet-text">${escape(tweet.content.text)}</p>
+    <footer>
+      <p>${timeago.format(tweet.created_at, 'en_US')}</p>
+      <div class="tweet-icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `);
+
+    return $tweet;
+  };
+
+  //submit event listener 
+  $("#tweet-form").on("submit", function(event) {
+    // prevent the default browser behavior
+    // to create a new tweet page.
+    event.preventDefault();
+    $characterCount.text("140");
+
+    $(".te-error").slideUp();
+    const tweetVal = $("#tweet-text").val();
+
+    if (!tweetVal) {
+      $("#no-content").slideDown();
+      return;
+    }
+    if (tweetVal.length > 140) {
+      $("#too-many-chars").slideDown();
+      return;
+    }
+
+    const serializeData = $(this).serialize();
+    console.log(serializeData);
+
+
+    // AJAX POST/GET requestS
+    $.ajax({
+      url: "/tweets",
+      type: "POST",
+      data: serializeData,
+    })
+      .then((response) => {
+        $("#tweet-text").val("");
+        loadTweets();
+        console.log("response:", response);
+      })
+      .catch((error) => {
+        console.log("error:", error);
+      });
+
+    const loadTweets = function() {
+      // GET request
+      $.ajax({
+        url: "/tweets",
+        type: "GET",
+      })
+        .then(function(tweets) {
+          $(".tweet-list").empty();
+          renderTweets(tweets);
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+
+    };
+    loadTweets();
+
+  });
+});
